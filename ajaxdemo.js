5b6e399c7d027ae760bce89150cc09fa2d0b769a
sagor1155 <sagor.eee.cuet@gmail.com>
2020-03-31 01:36:42 +0600

    demo ajax get request

---
+++ ajaxdemo.js
@@ -0,0 +1,40 @@
+
+$(document).ready(function(){
+    var pageCounter = 1;
+    $("#butt").click(function(event){
+        event.preventDefault();
+        var ourRequest = new XMLHttpRequest();
+        ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
+        ourRequest.onload = function(){
+            if(ourRequest.status >= 200 && ourRequest.status < 400){
+                var ourData = JSON.parse(ourRequest.responseText);
+                renderHTML(ourData);
+            }else{
+                console.error('Connected to server but returned an error!');
+            }
+        };
+        ourRequest.onerror = function(){
+            console.error('Connection error!');
+        };
+        ourRequest.send();
+        pageCounter++;
+        if(pageCounter > 3){
+            pageCounter = 1;
+        }
+    });
+
+    function renderHTML(data){
+        var htmlString = "";
+        for(var i=0; i<data.length; i++){
+            htmlString += '<p>' + data[i].name + ' is a ' + data[i].species + ' that likes to eat ';
+            for(var j=0; j<data[i].foods.likes.length; j++){
+                htmlString += data[i].foods.likes[j];
+                if(j != data[i].foods.likes.length-1){
+                    htmlString += ", ";
+                }
+            }
+            htmlString += '.</p>';
+        }
+        $("#animal-info").html($("#animal-info").html() + htmlString);
+    }
+});
